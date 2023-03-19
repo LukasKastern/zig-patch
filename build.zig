46fb67730581131f7dbcc391f038605c81aca8fc
Lukas Kastern <lukaskastern@web.de>
2023-03-19 11:38:59 +0100

    Fetch brotli via zig's package manager

--- build.zig
+++ build.zig
@@ -12,60 +12,22 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardOptimizeOption(.{});
 
-    // var brotli = b.addStaticLibrary(.{ .name = "Brotli", .target = target, .optimize = .ReleaseFast });
+    var brotli_dep = b.dependency("brotli", .{
+        .target = target,
+        .optimize = mode,
+    });
 
-    // brotli.addCSourceFiles(&.{
-    //     "third_party\\brotli-master\\c\\common\\constants.c",
-    //     "third_party\\brotli-master\\c\\common\\context.c",
-    //     "third_party\\brotli-master\\c\\common\\dictionary.c",
-    //     "third_party\\brotli-master\\c\\common\\platform.c",
-    //     "third_party\\brotli-master\\c\\common\\shared_dictionary.c",
-    //     "third_party\\brotli-master\\c\\common\\transform.c",
-
-    //     "third_party\\brotli-master\\c\\dec\\bit_reader.c",
-    //     "third_party\\brotli-master\\c\\dec\\decode.c",
-    //     "third_party\\brotli-master\\c\\dec\\huffman.c",
-    //     "third_party\\brotli-master\\c\\dec\\state.c",
-
-    //     "third_party\\brotli-master\\c\\enc\\backward_references_hq.c",
-    //     "third_party\\brotli-master\\c\\enc\\backward_references.c",
-    //     "third_party\\brotli-master\\c\\enc\\bit_cost.c",
-    //     "third_party\\brotli-master\\c\\enc\\block_splitter.c",
-    //     "third_party\\brotli-master\\c\\enc\\brotli_bit_stream.c",
-    //     "third_party\\brotli-master\\c\\enc\\cluster.c",
-    //     "third_party\\brotli-master\\c\\enc\\command.c",
-    //     "third_party\\brotli-master\\c\\enc\\compound_dictionary.c",
-    //     "third_party\\brotli-master\\c\\enc\\compress_fragment_two_pass.c",
-    //     "third_party\\brotli-master\\c\\enc\\compress_fragment.c",
-    //     "third_party\\brotli-master\\c\\enc\\dictionary_hash.c",
-    //     "third_party\\brotli-master\\c\\enc\\encode.c",
-    //     "third_party\\brotli-master\\c\\enc\\encoder_dict.c",
-    //     "third_party\\brotli-master\\c\\enc\\entropy_encode.c",
-    //     "third_party\\brotli-master\\c\\enc\\fast_log.c",
-    //     "third_party\\brotli-master\\c\\enc\\histogram.c",
-    //     "third_party\\brotli-master\\c\\enc\\literal_cost.c",
-    //     "third_party\\brotli-master\\c\\enc\\memory.c",
-    //     "third_party\\brotli-master\\c\\enc\\metablock.c",
-    //     "third_party\\brotli-master\\c\\enc\\static_dict.c",
-    //     "third_party\\brotli-master\\c\\enc\\utf8_util.c",
-    // }, &.{});
-    // brotli.addIncludePath("third_party\\brotli-master\\c\\include");
-    // brotli.linkLibC();
-    // brotli.install();
-
-    var brotli_dep = b.dependency("brotli", .{});
     var brotli_common = brotli_dep.artifact("brotlicommon");
     var brotli_enc = brotli_dep.artifact("brotliencoder");
     var brotli_dec = brotli_dep.artifact("brotlidec");
 
     const exe = b.addExecutable(.{ .name = "zig-patch", .root_source_file = .{ .path = "src/main.zig" }, .target = target, .optimize = mode });
 
-    exe.addIncludePath("third_party\\brotli-master\\c\\include");
     exe.addIncludePath("third_party\\zig-zlib\\zlib");
+
     exe.install();
 
     var lib = zlib.create(b, target, mode);
-    // exe.linkLibrary(brotli);
     exe.linkLibrary(lib.step);
     exe.linkLibrary(brotli_common);
     exe.linkLibrary(brotli_enc);
@@ -80,37 +42,27 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const test_exe = b.addTest(.{ .name = "test", .target = target, .optimize = mode, .kind = .test_exe, .root_source_file = .{ .path = "src/main.zig" } });
-    // test_exe.linkLibrary(brotli);
+    const test_exe = b.addTest(.{ .name = "test", .target = target, .optimize = mode, .root_source_file = .{ .path = "src/main.zig" } });
+    exe.linkLibrary(brotli_common);
+    exe.linkLibrary(brotli_enc);
+    exe.linkLibrary(brotli_dec);
+
+    test_exe.addIncludePath("third_party\\zig-zlib\\zlib");
+    test_exe.linkLibrary(lib.step);
     test_exe.linkLibrary(brotli_common);
     test_exe.linkLibrary(brotli_enc);
     test_exe.linkLibrary(brotli_dec);
 
-    // test_exe.addIncludePath("third_party\\brotli-master\\c\\include");
-    test_exe.addIncludePath("third_party\\zig-zlib\\zlib");
-
-    test_exe.linkLibrary(lib.step);
-
     const install_test = b.addInstallArtifact(test_exe);
 
     { // >>> TEST - BUILD ONLY >>>
-        // test_exe.setTarget(target);
-        // test_exe.setBuildMode(mode);
-
         const install_test_step = b.step("build-test", "Build unit tests");
         install_test_step.dependOn(&install_test.step);
-
-        //const main_tests = b.addTest("src/tests.zig");
-        //install_test_step.dependOn(&main_tests.step);
     } // <<< TEST - BUILD ONLY <<<
 
     { // >>> TEST - BUILD AND RUN >>>
         const run_test_cmd = test_exe.run();
         run_test_cmd.step.dependOn(&install_test.step);
-
-        // Test executables need the zig command/path as the first argument.
-        // Not passing it here because it's done implicitely.
-        // run_test_cmd.addArg("zig");
 
         const run_test_step = b.step("test", "Run unit tests");
         run_test_step.dependOn(&run_test_cmd.step);
